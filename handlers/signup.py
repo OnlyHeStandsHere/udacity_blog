26fead6781e734ee18ffb77e94929b9818cf6cf5
Jesse Maitland <theglobaltradingpost@yahoo.ca>
2017-05-06 15:24:19 -0700

    added signup.py

---
+++ handlers/signup.py
@@ -0,0 +1,48 @@
+from blog import BlogHandler
+from models.models import User
+from helpers import app
+import re
+
+USER_RE = re.compile("^[a-zA-Z0-9_-]{3,20}$")
+PASSWORD_RE = re.compile("^.{3,20}$")
+EMAIL_RE = re.compile("^[\S]+@[\S]+.[\S]+$")
+
+
+class SignupHandler(BlogHandler):
+    def get(self):
+        self.render("signup.html", errors={})
+
+    def post(self):
+        user_name = self.request.get("username")
+        password = self.request.get("password")
+        verify = self.request.get("verify")
+        email = self.request.get("email")
+        errors = {}
+
+        if not USER_RE.match(user_name):
+            errors["user"] = "A user name is required"
+        else:
+            user = User.fetch_by_name(user_name)
+            if user:
+                errors["user"] = "user name already exists"
+
+        if not PASSWORD_RE.match(password):
+            errors["password"] = "A password is required"
+        elif password != verify:
+            errors["verify"] = "Passwords do not match"
+
+        if email:
+            if not EMAIL_RE.match(email):
+                errors["email"] = "An email address is required"
+
+        if errors:
+            self.render("signup.html", user_name=user_name, email=email, errors=errors)
+        else:
+            password_hash = app.hash_password(password)
+            user = User(user_name=user_name, password=password_hash, email=email)
+            user.put()
+            user_id = str(user.key().id())
+            user_hash = app.make_secure_cookie(user_id)
+            self.response.set_cookie("user_id", user_hash)
+            self.redirect('/welcome')
+
